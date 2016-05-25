4f6cb4213f7d272579ccd40636c4d4bd723b7a1a
byron1st <byron1st@icloud.com>
2016-05-26 00:04:36 +0900

    details 쪽 뷰들 파일들 생성

---
+++ src/components/exdef.info.view.jsx
@@ -0,0 +1,12 @@
+'use strict'
+
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+export default class ExdefDetailsInfo extends Component {
+  render () {
+    return (
+      <h1>Info View</h1>
+    )
+  }
+}
