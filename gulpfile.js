029662db7115d7c5aa392df3d0a8426befa534c6
byron1st <byron1st@icloud.com>
2016-07-11 12:06:58 +0900

    불필요한 lib, code 정리

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,6 @@ gulp.task('clean', () => {
   ])
 })
 
-gulp.task('copy:bootstrap:js', () => {
-  return gulp.src([
-    './node_modules/bootstrap/dist/js/bootstrap.min.js'
-  ])
-  .pipe(gulp.dest('./dist/lib/bootstrap/js'))
-})
-
-gulp.task('copy:bootstrap:css', () => {
-  return gulp.src([
-    './node_modules/bootstrap/dist/css/bootstrap.min.css'
-  ])
-  .pipe(gulp.dest('./dist/lib/bootstrap/css'))
-})
-
-gulp.task('copy:bootstrap:fonts', () => {
-  return gulp.src([
-    './node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}'
-  ])
-  .pipe(gulp.dest('./dist/lib/bootstrap/fonts'))
-})
-
-gulp.task('copy:vis', () => {
-  return gulp.src([
-    './node_modules/vis/dist/vis.min.css',
-    './node_modules/vis/dist/vis.min.js'
-  ])
-  .pipe(gulp.dest('./dist/lib/vis'))
-})
-
-gulp.task('copy:vis:img', () => {
-  return gulp.src([
-    './node_modules/vis/dist/img/*/*'
-  ])
-  .pipe(gulp.dest('./dist/lib/vis/img'))
-})
-
 gulp.task('compile:js', () => {
   return gulp.src([
       './src/*.js',
@@ -64,4 +28,4 @@ gulp.task('copy:view', () => {
   .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('default', ['clean', 'compile:js', 'copy:bootstrap:js', 'copy:bootstrap:css', 'copy:bootstrap:fonts', 'copy:vis', 'copy:vis:img', 'copy:view'])
+gulp.task('default', ['clean', 'compile:js', 'copy:view'])
